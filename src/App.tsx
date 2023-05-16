394e31ea03b2dfd8614095c8e70ef0991f2bca09
Maven <jianwen@arctuition.com>
2023-05-16 18:29:35 +0800

    done table

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Table, { TableColumn } from "./Table";
+interface TableData {
+  id: number,
+  name: string,
+  age: number,
+  gender: string,
+}
+
+const data: TableData[] = [];
+for (let i = 0; i < 100; i++) {
+  data.push({
+    id: i,
+    name: `Edward ${i}`,
+    
+    age: i + 1,
+    gender: `male. ${i}`,
+  });
+}
+
+const columns: TableColumn<TableData>[] = [
+  { key: 'id', title: 'ID', fixed: 'left', width: 100 },
+  { key: 'name', title: 'Name', fixed: 'left', width: 100 },
+  { key: 'age', title: 'Age', sortable: true,  },
+  { title: 'Column1', key: 'name'},
+  { title: 'Column 2', key: 'name' },
+  { title: 'Column 3', key: 'name' },
+  { title: 'Column 4', key: 'name' },
+  { title: 'Column 5', key: 'name' },
+  { title: 'Column 6', key: 'name' },
+  { title: 'Column 7', key: 'name' },
+  { title: 'Column 8', key: 'name', width: 100 },
+  { key: 'gender', title: 'Gender', fixed: 'right', width: 100 },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Table
+      data={data}
+      columns={columns}
+      defaultSortColumn="age"
+      defaultSortDirection="asc"
+      pageSize={10}
+      scroll={{ x: 1500, y: 150 }}
+      size="small"
+    />
   );
 }
 
